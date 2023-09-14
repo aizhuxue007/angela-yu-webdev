f5998502cfc0a5bd5cc6c1c1d0eefdf246053960
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-14 11:22:22 -0400

    updated slightly

---
+++ 100Devs/hw/class-12/javascript.info variables/index.js
@@ -0,0 +1,18 @@
+// Tasks
+
+// Working with variables
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+alert(name)
+
+// Giving the right name
+let ourPlanet = 'earth';
+let currentUser = 'Peter';
+
+// Uppercase const?
+// - Birthday is hard-coded so Const makes sense
+// - Age is calculated so it will be changed. Using let will make sense.
