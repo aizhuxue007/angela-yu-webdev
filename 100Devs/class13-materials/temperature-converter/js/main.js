0d0afba624d359b48b3a14d191c738df03ee4373
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-16 08:45:09 -0400

    finished js

--- 100Devs/class13-materials/temperature-converter/js/main.js
+++ 100Devs/class13-materials/temperature-converter/js/main.js
@@ -1,13 +1,31 @@
-//Write your pseduo code first! 
-// 1. get the values 
-// 2. convert the c to f using math formula, store in variable result
-// 3. return the result
+// select celcius input, fahrenheit button, output
+const celsiusInput = document.getElementById('celcius')
+const toFahrenheitBtn = document.getElementById('f')
+const output = document.getElementById('output')
 
-const cInput = document.getElementById('celcius')
-const fBtn = document.getElementById('f').addEventListener('click', convertToF)
+// add click listener to fahrenheit button that calls handleClick
+toFahrenheitBtn.addEventListener('click', handleClick)
 
-function convertToF() {
-    let c = cInput.value;
-    const output = document.getElementById('output')
-    output.innerText = parseInt(c) * 9/5 + 32;
+// handleClick()
+function handleClick() {
+    // store convertToF(c) to f
+    let f = convertToF(celsiusInput.value)
+    // render(f)
+    render(f)
+}
+
+function convertToF(c) {
+// convertToF(c)
+    return c * 9/5 + 32;
+
+}
+
+// render(text)
+function render(txt) {
+    // output text set to text
+    output.innerText = txt
+}
+
+function log(item) {
+    console.log(item)
 }
