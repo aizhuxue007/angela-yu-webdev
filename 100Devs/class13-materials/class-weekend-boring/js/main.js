362e8b905591d74832ffb7b9ebb7e63101025c4c
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-16 08:29:11 -0400

    finished class-weekend-boring

--- 100Devs/class13-materials/class-weekend-boring/js/main.js
+++ 100Devs/class13-materials/class-weekend-boring/js/main.js
@@ -1,17 +1,40 @@
 //Handle Capital Letters, Place result in DOM, add a check for humpday (Wed)
 
-document.querySelector('#check').addEventListener('click', check)
+// select day input, check btn, and #placeToSee
+const dayInput = document.getElementById('day')
+const checkBtn = document.getElementById('check')
+const output = document.getElementById('placeToSee')
 
+// add click listener to checkBtn that runs check()
+checkBtn.addEventListener('click', check)
+
+// check()
 function check() {
-
-  const day = document.querySelector('#day').value
-
-  if(day === "tuesday" || day === "thursday"){
-    console.log("YOU HAVE CLASS")
-  }else if( day === "saturday" || day === "sunday"){
-    console.log("Its The Weekend")
-  }else{
-    console.log("BORING")
+  // let day be the value of lowercase dayInput
+  let day = dayInput.value.toLowerCase()
+  // if day is tuesday or day is thursday
+  if (day === 'tuesday' || day === 'thursday') { 
+    // output shows "Class Time"
+    render('Class Time!')
+  } else if (day === 'wednesday') {
+    render('HUMPDay!')
+  } else if (day.includes('day')) {
+    render('BOOORRRRIIINNNNGG')
+  } else {
+    render('HUUHHH?? UHM WHAT?!')
   }
+  // else if day is wednesday
+    // output shows "Humpday!"
+  // else if day contains "day"
+    // output "BOOOORRRRIIINNNG!"
+  // else
+    // output "HUUHHH?? UHM WHAT?!"
+}
 
+function render(text) {
+  output.innerText = text
+}
+
+function log(obj) {
+  console.log(obj)
 }
