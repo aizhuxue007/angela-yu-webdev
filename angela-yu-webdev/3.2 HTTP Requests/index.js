0e88157867446ee02ab7f9b0d54dac4c7e74e755
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-18 06:26:12 -0400

    done with 3.2

--- angela-yu-webdev/3.2 HTTP Requests/index.js
+++ angela-yu-webdev/3.2 HTTP Requests/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
-    console.log(req)
+    console.log(req.rawHeaders)
+    res.send('<h1>HELLO WORLD!</h1>');
 })
 
-app.listen(port, () => {
-    console.log(`Listening at PORT: ${port}.`)
+app.get('/contact', (req, res) => {
+    res.send('<h2>333-058-2941</h2>');
+})
+
+app.get('/about', (req, res) => {
+    res.send('<p>I love Jesus because He first loved me</p>');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is servicing on port ${PORT}`)
 });
