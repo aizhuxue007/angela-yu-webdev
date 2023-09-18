7365c22cfc7f98079fc3ffc547b7fadd31204d17
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-18 05:17:07 -0400

    learning about http requests

---
+++ angela-yu-webdev/3.2 HTTP Requests/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    console.log(req)
+})
+
+app.listen(port, () => {
+    console.log(`Listening at PORT: ${port}.`)
+});
