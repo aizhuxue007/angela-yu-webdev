c5f9d347205416e67a12d822b6796ebe86e3e98e
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-19 08:01:16 -0400

    4.2 exercise

--- angela-yu-webdev/4.2 Passing Data/index.js
+++ angela-yu-webdev/4.2 Passing Data/index.js
@@ -3,12 +3,27 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+var numOfLetters = null;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(checkInputLength)
 
-app.get("/", (req, res) => {});
+function checkInputLength(req, res, next) {
+  if (req.body.fName != undefined && req.body.lName != undefined) {
+    numOfLetters = req.body.fName.length + req.body.lName.length
+  }
+  next();
+}
 
-app.post("/submit", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render('index.ejs')
+});
+
+app.post("/submit", (req, res) => {
+  res.render('index.ejs', 
+    { letters: numOfLetters }
+  )
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
