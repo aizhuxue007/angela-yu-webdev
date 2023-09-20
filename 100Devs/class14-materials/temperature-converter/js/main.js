f8d0e58f97a3ac9200dfd4cea11f447c86918c55
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-20 08:46:10 -0400

    done temp convert

--- 100Devs/class14-materials/temperature-converter/js/main.js
+++ 100Devs/class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,22 @@
 //Write your pseduo code first! 
+
+// set up a clicklistener that calls outputF
+document.getElementById('toF').addEventListener('click', outputF)
+
+// in outputF
+    // set up a variable c which is the input value
+    // return the conversion math f = c * 9/5 + 32;
+    // innerText = conversion
+function outputF() {
+    const c = document.getElementById('celsius').value;
+    render(toF(c))
+}
+
+function render(txt) {
+    const output = document.getElementById('output')
+    output.innerText = txt
+}
+
+function toF(c) {
+    return c * 9/5 + 32;
+}
