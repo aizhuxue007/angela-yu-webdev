8504efd48cce31d328a99a73c7a93c938cadb85b
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-21 16:53:31 -0400

    got nikki to show up

--- 100Devs/class14-materials/bachelor-code/1/js/main.js
+++ 100Devs/class14-materials/bachelor-code/1/js/main.js
@@ -4,3 +4,13 @@
 // 	document.querySelector('#claire').style.display = 'none'
 // 	document.querySelector('#sharleen').style.display = 'none'
 // }
+
+console.log('in here');
+
+document.getElementById('finalRose').addEventListener('click', run)
+
+function run() {
+	console.log('clicked')
+	document.getElementById('claire').style.display = 'none';
+	document.getElementById('sharleen').style.display = 'none';
+}
