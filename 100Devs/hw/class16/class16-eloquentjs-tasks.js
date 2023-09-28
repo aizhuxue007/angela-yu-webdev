ab2295d4398795484ab477643bff6e1c261cf615
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-28 06:19:42 -0400

    finsihed eloquentjs

--- 100Devs/hw/class16/class16-eloquentjs-tasks.js
+++ 100Devs/hw/class16/class16-eloquentjs-tasks.js
@@ -54,11 +54,13 @@ console.log(col_str);
 // The previous chapter introduced the standard function Math.min that returns its smallest argument. We can build something like that now. Write a function min that takes two arguments and returns their minimum.
 
 // // Your code here.
-
+min = (a, b) => Math.min(a, b);
 // console.log(min(0, 10));
 // // → 0
+console.log(min(0, 10))
 // console.log(min(0, -10));
 // // → -10
+console.log(min(0, -10))
 
 // Recursion
 // We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
@@ -70,21 +72,45 @@ console.log(col_str);
 // For any other number N, its evenness is the same as N - 2.
 
 // Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
+isEven = (n) => {
+    if (n === 0) return true;
+    if (n === 1) return false;
+    if (n < 0) return n % 2 === 0;
+    return isEven(n - 2);
+}
 
 // Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+console.log(isEven(-2));
+// maximum call stack is exceeded because the negative number never equals to 0 or 1. I just added another if statement that checks if % 2 === 0
 
-1
 
 // Bean counting
 // You can get the Nth character, or letter, from a string by writing "string"[N]. The returned value will be a string containing only one character (for example, "b"). The first character has position 0, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
 
 // Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters there are in the string.
-
+function countBs(str) {
+    return countChar(str, 'B');
+}
 // Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
 
 // // Your code here.
+function countChar(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
+        }
+    }
+    return count 
+}
 
 // console.log(countBs("BBC"));
 // // → 2
 // console.log(countChar("kakkerlak", "k"));
 // // → 4
+
+console.log(countBs("BBC"));
+console.log(countChar("kakkerlak", "k"));
