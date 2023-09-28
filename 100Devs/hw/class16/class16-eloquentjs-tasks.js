61e0e0471034ac2d8a89f51276f7aa01a0ecdffc
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-27 22:29:55 -0400

    finished

--- 100Devs/hw/class16/class16-eloquentjs-tasks.js
+++ 100Devs/hw/class16/class16-eloquentjs-tasks.js
@@ -25,9 +25,27 @@ for (let i = 1; i <= 7; i++) {
 // When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 
 // (This is actually an interview question that has been claimed to weed out a significant percentage of programmer candidates. So if you solved it, your labor market value just went up.)
+for (let i = 1; i <= 100; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) console.log('FizzBuzz'); 
+    else if (i % 3 === 0) console.log('Fizz');
+    else if (i % 5 === 0) console.log('Buzz');
+    else console.log(i)
+}
 
 
 // Chessboard
 // Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
 
 // Passing this string to console.log should show something like this:
+
+let col_str = ''
+
+for (let row = 1; row <= 8; row++) {
+    for (let col = 1; col <= 8; col++) {
+        if ((row + col) % 2 === 0) col_str += ' ';
+        else col_str += '#';
+    }
+    col_str += '\n';
+}
+
+console.log(col_str);
