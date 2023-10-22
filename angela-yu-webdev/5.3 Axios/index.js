80c5f6484f6699e07ee6b304ba8e217c950fce1e
aizhuxue007 <aizhuprogram@gmail.com>
2023-10-22 08:17:12 -0400

    everything

--- angela-yu-webdev/5.3 Axios/index.js
+++ angela-yu-webdev/5.3 Axios/index.js
@@ -15,6 +15,7 @@ app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
+    console.log(result)
     res.render("index.ejs", { data: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
@@ -30,6 +31,17 @@ app.post("/", async (req, res) => {
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
   // sure you're passing both the type and participants queries.
+  try {
+    const resp = await axios.get(`https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`)
+    const result = resp.data
+    console.log(result)
+    res.render("index.ejs", {data: result[0]})
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
   // Render the index.ejs file with a single *random* activity that comes back
   // from the API request.
   // Step 3: If you get a 404 error (resource not found) from the API request.
