11263abd764f65dce9ed2b0d3b2042d53db94fa1
aizhuxue007 <aizhuprogram@gmail.com>
2023-10-23 11:05:27 -0400

    stuff

---
+++ 100Devs/class21-materials/mighty-morphin-review/js/main.js
@@ -0,0 +1,40 @@
+// *Variables*
+// Declare a variable, reassign it to your fav holiday, make sure it is in all caps, and print the value to the console
+let favHoliday = 'CHRISTMAS'.toUpperCase();
+console.log(favHoliday);
+
+//Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
+let str = 'last threee letters';
+alert(str.split('').slice(-3).join(""));
+
+// *Functions*
+// Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
+function absolute(n1, n2, n3, n4, n5) {
+    alert(100 - n1 - n2 - n3 - n4 - n5);
+}
+
+absolute(10, 10, 10, 10, 10);
+// Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
+function highAndLow(n1, n2, n3) {
+    console.log(Math.min(n1, n2, n3),Math.max(n1, n2, n3))
+}
+
+highAndLow(1, 2, 3);
+
+
+// *Conditionals*
+//Create a function that returns heads or tails randomly and as fairly as possible. Call the function.
+function headsOrTails() {
+    return Math.random() >= 0.50 ? 'heads' : 'tails';
+}
+
+console.log(headsOrTails());
+//*Loops*
+//Create a function that takes in a number. Console log the result of heads or tails using the previous function x times where x is the number passed into the function. Call the function.
+function headOrTailsTimes(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(i, headsOrTails())
+    }
+}
+
+headOrTailsTimes(5);
